e84c98c3dfdab7195796d5461d0b2f66e82822d2
mkaabouch <mehdi.kaabouch.pro@gmail.com>
2023-01-19 21:44:16 +0100

    Error controller

---
+++ Medhead API/medhead-core/src/main/java/com/medhead/api/controller/CustomErrorController.java
@@ -0,0 +1,15 @@
+package com.medhead.api.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CustomErrorController implements ErrorController {
+    @RequestMapping(value = "/error")
+    public String error()
+    {
+        return "Error when accessing Medhead API. Please check your request.";
+    }
+
+}
