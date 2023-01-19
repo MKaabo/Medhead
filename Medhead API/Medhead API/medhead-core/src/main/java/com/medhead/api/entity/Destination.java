031466ebdf7c25752968a8e2cfb8a712dd5e6a81
mkaabouch <mehdi.kaabouch.pro@gmail.com>
2023-01-19 18:07:21 +0100

    adding dto, mapper, fixing bugs, Mabox API..

--- Medhead API/Medhead API/medhead-core/src/main/java/com/medhead/api/entity/Destination.java
+++
@@ -1,9 +0,0 @@
-public class Destination
-{
-
-}
-
-/* HttpHeaders headers = new HttpHeaders();
-headers.setContentType(MediaType.APPLICATION_JSON);
-HttpEntity<String> entity = new HttpEntity<String>(requestJson, headers);
-ResponseEntity<String> response = restTemplate.put(url, entity); */
